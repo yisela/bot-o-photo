83dfa61bd711eff7d4e217c9db017f3a06b06abe
Yisela Alvarez Trentini <utopica@gmail.com>
2017-05-10 14:07:00 +0200

    Uploaded base files

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         '5lRpAPIS6utvhEyv23tJgkzZ1',
+  consumer_secret:      'zXS3Yf8SbIK4Nqlrzy3NEsDEXEjQbHZscP2455S5JdexvjqRG5',
+  access_token:         '861634677337182215-Kgdph7Rx50VW7PWDwfyngx2kahYppDQ',
+  access_token_secret:  'QuH2hSMsUG323wJeHsPdxgLO6aDHUUodfKq522hxTgp0n'
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,62 @@
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+// pick an image from a folder at random
+function random_from_array(images){
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+// upload random image
+function upload_random_image(images){
+  console.log('Opening an image...');
+  var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+      b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
+      console.log(err);
+    }
+    else{
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('statuses/update', {
+        media_ids: new Array(data.media_id_string)
+      },
+        function(err, data, response) {
+          if (err){
+            console.log('ERROR:');
+            console.log(err);
+          }
+          else{
+            console.log('Posted an image!');
+          }
+        }
+      );
+    }
+  });
+}
+
+// see what files are in our images directory
+fs.readdir(__dirname + '/images', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var images = [];
+    files.forEach(function(f) {
+      images.push(f);
+    });
+
+    setInterval(function(){
+      upload_random_image(images);
+    }, 10000);
+  }
+});
